6ec96a92c6a47937431971f9777803e38ee5ca24
SINDHUJA <165631602+Sindhuja-Shanmugam@users.noreply.github.com>
2025-06-16 10:29:59 +0530

    Add files via upload

---
+++ Frontend/src/context/leaveBalane.js
@@ -0,0 +1,28 @@
+const leaveBalance = [
+    {
+  sick: 4,
+  casual: 6,
+  paid: 8,
+  lop:0,
+  floating: 5,
+  maternity: 0,
+    },
+  {
+    sick: 5,
+  casual: 3,
+  paid: 0,
+  lop:0,
+  floating: 3,
+  maternity: 0,
+  },
+  {
+    sick: 3,
+  casual: 0,
+  paid: 0,
+  lop:0,
+  floating: 1,
+  maternity: 0,
+  },
+]
+
+export default leaveBalance;
---
+++ Frontend/src/context/teamData.js
@@ -0,0 +1,58 @@
+const team = [
+  {
+    id: 11,
+    name: "sindhuja",
+    email: "sindhu@lumel.com",
+    emp_type_id: 5,
+  },
+  {
+    id: 12,
+    name: "Krish",
+    email: "krish@lumel.com",
+    emp_type_id: 6,
+  },
+  {
+    id: 13,
+    name: "John Doe",
+    email: "john.doe@example.com",
+    emp_type_id: 6,
+  },
+  {
+    id: 14,
+    name: "John",
+    email: "john@example.com",
+    emp_type_id: 6,
+  },
+  {
+    id: 15,
+    name: "Sudha",
+    email: "sudha@example.com",
+    emp_type_id: 6,
+  },
+  {
+    id: 17,
+    name: "sarve",
+    email: "sarve@example.com",
+    emp_type_id: 6,
+  },
+  {
+    id: 19,
+    name: "sarve1",
+    email: "sarve1@example.com",
+    emp_type_id: 6,
+  },
+  {
+    id: 20,
+    name: "sarve12",
+    email: "sarve12@example.com",
+    emp_type_id: 6,
+  },
+  {
+    id: 22,
+    name: "sarve123",
+    email: "sarve123@example.com",
+    emp_type_id: 6,
+  },
+];
+
+export default team;
